f8ade4c0eb8cc3bd130be19ed09d33ca087e0048
WiLLLLL683 <32411970+WiLLLLL683@users.noreply.github.com>
2024-08-05 16:46:02 +0800

    Pathfinding for enemies

--- Assets/Scripts/Actors/Enemy.cs
+++ Assets/Scripts/Actors/Enemy.cs
@@ -7,12 +7,17 @@ namespace TopDownShooter
 {
     public class Enemy : MonoBehaviour
     {
-        [SerializeField] private NavMeshAgent navMeshAgent;
         [SerializeField] private HealthBase health;
+        [SerializeField] private float smoothTime;
 
         private EnemyConfig config;
         private Transform target;
         private ScoreService scoreService;
+        private NavMeshPath path;
+        private Vector3 velocity = Vector3.zero;
+
+        private const float cornerCheckTolerance = 0.6f;
+        private int nextCorner;
 
         public void Init(EnemyConfig config, Transform target, ScoreService scoreService)
         {
@@ -32,15 +37,59 @@ namespace TopDownShooter
 
         private void Update()
         {
-            MoveToTarget();
+            FindPath();
+            MoveAlongPath();
         }
 
-        private void MoveToTarget()
+
+        private void FindPath()
         {
             if (target == null)
                 return;
 
-            navMeshAgent.SetDestination(target.position);
+            path = new();
+            if (!NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path))
+            {
+                path = null;
+            }
+            nextCorner = 0;
+        }
+
+        private void MoveAlongPath()
+        {
+            if (path == null)
+                return;
+            if (path.status == NavMeshPathStatus.PathInvalid)
+                return;
+
+            nextCorner = FindClosestCorner();
+
+            if ((transform.position - path.corners[nextCorner]).magnitude <= cornerCheckTolerance)
+            {
+                nextCorner++;
+            }
+
+            Vector3 target = path.corners[nextCorner];
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime, config.moveSpeed);
+        }
+
+        private int FindClosestCorner()
+        {
+            int closestCorner = path.corners.Length - 1;
+            float closestDistance = (path.corners[closestCorner] - transform.position).magnitude;
+
+            for (int i = path.corners.Length - 1; i > 0; i--)
+            {
+                float distance = (path.corners[i] - transform.position).magnitude;
+
+                if (distance < closestDistance)
+                {
+                    closestCorner = i;
+                    closestDistance = distance;
+                }
+            }
+
+            return closestCorner;
         }
 
         private void Die()
